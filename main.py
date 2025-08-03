3394a1f8890f3dcc415926d13f60330e040fca9d
alexk <alexkamyshan5@gmail.com>
2025-08-03 11:43:03 +0300

    add timer

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import time
+
 questions = {'Сколько планет в Солнечной системе?':8,
              'Столица Франции?':'Париж'}
 score = 0
 
 for k, a in questions.items():
+    start_time = time.time()
     uans = input(k + '')
+    end_time = time.time()
+    if round(end_time - start_time, 1)>5:
+        print('Вы не успели')
+        uans = 'wrong'
+    print(f'Время ответа:{round(end_time - start_time, 1)}с.')
     if uans.lower() == a.lower():
         print('Правильно')
         score += 1
